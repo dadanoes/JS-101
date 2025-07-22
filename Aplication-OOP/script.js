9d5397a026dc5889eb49055c87806af9dbde85ab
Dadan Nurohman <38880382+dadanoes@users.noreply.github.com>
2025-07-23 01:45:27 +0700

    menambahkan file ke aplication-oop

---
+++ Aplication-OOP/script.js
@@ -0,0 +1,32 @@
+class Product {
+  constructor(id, name, description, price, image) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.image = image;
+  }
+  displayCard() {
+    return `<img src="${this.image}" alt="${this.name}">
+    <h3>${this.name}</h3>
+    <p>${this.price.toFixed(2)}</p> 
+    <p id="quantity${this.id}">Quantity: 0</p>
+    <button onclick="shopingCart.viewProductDetail(${
+      this.id
+    })>View Details</button>
+    <button onclick="shopingCart.addToCart(${
+      this.id
+    }, ${true})">Add to Cart</button>`;
+  }
+  // toFixed(2) untuk menampilkan 2 angka di belakang koma
+}
+class CartItem {
+  constructor(product, quantity) {
+    this.product = product;
+    this.quantity = quantity;
+  }
+  increment() {
+    this.quantity++;
+  }
+}
+class ShopingCart {}
